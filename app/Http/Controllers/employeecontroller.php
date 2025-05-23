658bddb616a77392f77479f0ebcc7f904d5c47f8
yumi-scarlet <yumikajuco@gmail.com>
2025-05-23 12:09:04 +0800

    final destination

---
+++ app/Http/Controllers/employeecontroller.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Fascades\DB;
+use Response;
+use Illuminate\Http\Request;
+use App\Models\employee;
+
+class employeecontroller extends Controller
+{
+    public function index()
+    {   
+        $employees = employee::get();
+
+        return view ('employee.index', compact('employees'));
+    }
+
+    public function create()
+    {
+        return view ('employee.create');
+    }
+
+
+    public function store(Request $request)
+    {
+    $request->validate([
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
+        
+    ]);
+
+    employee::create($request->all());
+    return view ('employee.create');
+    }
+
+    public function edit( int $id)
+    {
+        $employees = employee::find($id);
+        return view ('employee.edit',compact('employees'));
+    }
+
+    public function update(Request $request, int $id) {
+        {
+            $request->validate([
+                'fname' => 'required|max:255|string',
+                'lname' => 'required|max:255|string',
+                'midname' => 'required|max:255|string',
+                'age' => 'required|integer',
+                'address' => 'required|max:255|string',
+                'zip' => 'required|integer',
+                
+            ]);
+        
+            employee::findOrFail($id)->update($request->all());
+            return redirect ()->back()->with('status','Employee Updated Successfully!');
+            }
+    }
+
+    public function destroy(int $id){
+        $employees = employee::findOrFail($id);
+        $employees->delete();
+        return redirect ()->back()->with('status','Employee Deleted');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\employeecontroller;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::view('about', 'about')->name('about');
+    
+    Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
+    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index'])->name('employee.index');
+    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create'])->name('employee.create');
+    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.store');
+    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit'])->name('employee.edit');
+    Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update'])->name('employee.update');
+    Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'destroy'])->name('employee.delete');
+
+    
+
+
+
+
+
+
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+});
